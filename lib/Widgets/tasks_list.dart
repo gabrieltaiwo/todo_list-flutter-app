2519ccdd8c617814763f30d1a93adaba3f622a43
gabrieltaiwo <gabrieltaiwo@outlook.com>
2020-10-02 14:24:00 -0300

    initial commit

---
+++ lib/Widgets/tasks_list.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:todo_list/Widgets/task_tile.dart';
+import 'package:todo_list/models/task.dart';
+
+class TaskList extends StatefulWidget {
+  @override
+  _TaskListState createState() => _TaskListState();
+}
+
+class _TaskListState extends State<TaskList> {
+  List<Task> tasks = [
+    Task(name: 'Buy milk'),
+    Task(name: 'Buy egg'),
+    Task(name: 'Buy bread')
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        TaskTile(),
+        TaskTile(),
+        TaskTile(),
+      ],
+    );
+  }
+}
---
+++ lib/screens/add_task_screen.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class AddTaskScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Color(0xff757575),
+      child: Container(
+        padding: EdgeInsets.all(30.0),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20.0),
+            topRight: Radius.circular(20.0),
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              'Add Task',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.lightBlueAccent,
+                fontSize: 25.0,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+            TextField(
+              autofocus: true,
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            FlatButton(
+              onPressed: () {},
+              child: Text(
+                'Add',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              color: Colors.lightBlueAccent,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
